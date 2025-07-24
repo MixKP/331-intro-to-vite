75198478fdd52be20b5f3381191e9d6364ae3621
MixKP <peeranat_thiwongsa@cmu.ac.th>
2025-07-25 05:39:30 +0700

    Lab4 complete 1.6

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from './stores/message';
+import { storeToRefs } from 'pinia';
+
+const store = useMessageStore();
+const { message } = storeToRefs(store);
 </script>
 
 <template>
   <div id="layout">
     <header>
+      <div id="flashMessage" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
       <div class="wrapper">
         <nav>
           <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
@@ -40,5 +48,16 @@ nav a.router-link-exact-active {
 }
 h2 {
   font-size: 20px;
+}
+@keyframes yellowFlash {
+  from {
+    background-color: #f1c40f;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+#flashMessage {
+  animation: yellowFlash 1s ease-in-out;
 }
 </style>
--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -3,6 +3,7 @@ import { toRefs } from 'vue'
 import type { Event } from '@/types'
 import EventService from '@/services/EventService'
 import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
   event: Event
@@ -11,11 +12,17 @@ const props = defineProps<{
 
 const { event } = toRefs(props)
 const router = useRouter()
+const store = useMessageStore()
+
 const register = () => {
-  router.push({ name: 'event-detail-view'})
+  store.updateMessage('You are successfully registered for ' + props.event.title)
+  setTimeout(() => {
+    store.restoreMessage()
+  }, 3000)
+  router.push({ name: 'event-detail-view', params: { id: props.event.id } })
 }
 </script>
 <template>
   <p>Register event here</p>
-  <button @click="register">Register</button>
+  <button @click="register">Register Me!</button>
 </template>
