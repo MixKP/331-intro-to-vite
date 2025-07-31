a668ebb452e0b0d0baa32d91ab8d0f79d8a2dbb7
MixKP <peeranat_thiwongsa@cmu.ac.th>
2025-08-01 04:12:30 +0700

    complete13.6

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { createPinia } from 'pinia'
 
 import './assets/style.css'
 import 'nprogress/nprogress.css'
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+inject()
 
 app.use(createPinia())
 app.use(router)
