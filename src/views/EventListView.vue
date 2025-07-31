6eda54fe49f81c7f42f8df10eab2799a1ff30ad5
MixKP <peeranat_thiwongsa@cmu.ac.th>
2025-08-01 00:22:47 +0700

    complete 2.3

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -2,14 +2,13 @@
 import EventCard from '../components/EventCard.vue'
 import CategoryOrganizer from '@/components/CategoryOrganizer.vue'
 import EventService from '@/services/EventService'
-import { ref, onMounted, computed, watchEffect, watch} from 'vue'
+import { ref, onMounted, computed, watchEffect, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import type { Event } from '@/types'
 
 const events = ref<Event[] | null>(null)
 const totalEvents = ref(0)
 const router = useRouter() // Add this line
-
 
 const hasNextPage = computed(() => {
   const totalPages = Math.ceil(totalEvents.value / 3)
@@ -20,19 +19,19 @@ const handlePageSizeChange = (event: Event) => {
   const newSize = parseInt((event.target as HTMLSelectElement).value)
   router.push({
     name: 'event-list-view',
-    query: { page: 1, size: newSize }
+    query: { page: 1, size: newSize },
   })
 }
 
 const props = defineProps({
   page: {
     type: Number,
-    required: true
+    required: true,
   },
   size: {
     type: Number,
-    default: 3 // Default page size
-  }
+    default: 3, // Default page size
+  },
 })
 const page = computed(() => props.page)
 const pageSize = computed(() => props.size)
@@ -44,7 +43,7 @@ onMounted(() => {
         events.value = response.data
         totalEvents.value = parseInt(response.headers['x-total-count'])
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('There was an error! ', error)
       })
   })
@@ -57,33 +56,35 @@ onMounted(() => {
 
   <div class="page-size-selector">
     <label for="page-size">Events per page: </label>
-    <select
-      id="page-size"
-      :value="pageSize"
-      @change="handlePageSizeChange"
-      class="size-selector"
-    >
+    <select id="page-size" :value="pageSize" @change="handlePageSizeChange" class="size-selector">
       <option v-for="size in 10" :key="size" :value="size">
         {{ size }}
       </option>
     </select>
   </div>
 
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <div class="pagination">
-      <RouterLink id="page-prev" :to="{ name: 'event-list-view', query: { page: page - 1 } }" rel="prev" v-if="page != 1">&#60; Previous Page</RouterLink>
-      <RouterLink id="page-next" :to="{ name: 'event-list-view', query: { page: page + 1 } }" rel="next" v-if="hasNextPage">Next Page &#62;</RouterLink>
+      <RouterLink
+        id="page-prev"
+        :to="{ name: 'event-list-view', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous Page</RouterLink
+      >
+      <RouterLink
+        id="page-next"
+        :to="{ name: 'event-list-view', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next Page &#62;</RouterLink
+      >
     </div>
   </div>
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 .pagination {
   display: flex;
   width: 290px;
