1f516effaf93a294eb2126283923b0e225db692c
MixKP <peeranat_thiwongsa@cmu.ac.th>
2025-08-01 04:17:26 +0700

    fix deploy problem

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { createPinia } from 'pinia'
 import './assets/style.css'
 import 'nprogress/nprogress.css'
 import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 inject()
+injectSpeedInsights()
 
 app.use(createPinia())
 app.use(router)
