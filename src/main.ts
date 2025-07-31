62c74ede574fadbe507fdd0b301f359f9e470a80
MixKP <peeranat_thiwongsa@cmu.ac.th>
2025-08-01 00:15:02 +0700

    complete 1.7

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import './assets/style.css'
 import 'nprogress/nprogress.css'
 import App from './App.vue'
 import router from './router'
